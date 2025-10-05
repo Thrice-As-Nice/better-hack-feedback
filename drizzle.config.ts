a650af2d6d1e1590b037833920dd454311a7aef4
Dagim G. Astatkie <workflow.jd@gmail.com>
2025-10-05 08:05:19 +0300

    feat: add initial Drizzle configuration for SQLite database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
-  schema: './drizzle/schema.ts',
-  out: './drizzle/migrations',
+  schema: './server/drizzle/schema.ts',
+  out: './server/drizzle/migrations',
   dialect: 'sqlite',
   casing: 'snake_case',
   dbCredentials: {
-    url: './data/db.sqlite',
+    url: './server/data/db.sqlite',
   },
 })
